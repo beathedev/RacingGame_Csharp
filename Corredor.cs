0afb046378176b86774baa3e54b50d212ce1a43f
Beatriz Francisconi <beatrizfrancisconi@souunisuam.com.br>
2023-01-20 20:29:55 -0300

    Classe Corredor criada, metodos e construtores; Pista criada

---
+++ Corredor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RacingGame
+{
+    public class Corredor
+    {
+        private String nome;
+
+        public String Nome
+        {
+            get { return this.nome; }
+            set {
+                this.nome = value.ToLower(); 
+                if (value.Length == 0) this.nome = "Visitante";
+                if(value.Length > 1 ) this.nome = value[0].ToString().ToLower();
+            }
+        }
+
+
+        public Corredor()
+        {
+            this.Nome = "";
+        }
+
+        public Corredor(String nome)
+        {
+            this.Nome = nome;
+        }
+
+        public int Correr()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, 5);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace RacingGame
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            int tlPista = 100;
+            int posicaoC1 = 0;
+            String pista = "";
+            String corredorPista = "";
+
+
+            Corredor c1 = new Corredor();
+            c1.Nome = "D";
+            posicaoC1 = c1.Correr();
+
+
+            //Monta a imagem do corredor na pista
+            for (int i = 0; i < tlPista; i++)
+            {
+                pista += "_";
+                if(i == posicaoC1)
+                {
+                    corredorPista += c1.Nome;
+                }else
+                {
+                    corredorPista += " ";
+                }
+            }
+
+            //exibir a pista e o corredor
+            Console.WriteLine(corredorPista);
+            Console.WriteLine(pista);
+
+        }
+    }
+}
